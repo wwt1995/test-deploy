62f4281b00237cb6f6f0f37f0f2cb14d2ec41eea
Kurisu <289616584@qq.com>
2025-02-09 14:54:15 +0800

    chore: 测试cicd

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it 111111!" />
+      <HelloWorld msg="You did it 22222!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
